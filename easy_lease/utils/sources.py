44a66bcf40c9463eef2a4ad4f5cfcf33ca0ce395
binayachaudari <binayachaudari@live.com>
2024-02-24 23:56:27 -0500

    feat: chat API endpoint implemented

---
+++ easy_lease/utils/sources.py
@@ -0,0 +1,34 @@
+sources = [
+   {
+      "title":"Rights & Responsibilities for Landlords & Tenants",
+      "url":"https://www.toronto.ca/community-people/housing-shelter/rental-housing-tenant-information/rights-responsibilities-for-landlords-tenants/"
+   },
+   {
+      "title":"Rental Housing Standards",
+      "url":"https://www.toronto.ca/community-people/housing-shelter/rental-housing-tenant-information/rental-housing-standards/"
+   },
+   {
+      "title":"Understand & Fight Evictions",
+      "url":"https://www.toronto.ca/community-people/housing-shelter/rental-housing-tenant-information/understand-fight-evictions/"
+   },
+   {
+      "title":"Rent-Geared-to-Income Housing",
+      "url":"https://www.toronto.ca/community-people/employment-social-support/housing-support/rent-geared-to-income-subsidy/"
+   },
+   {
+      "title":"Financial Support for Renters",
+      "url":"https://www.toronto.ca/community-people/employment-social-support/housing-support/financial-support-for-renters/"
+   },
+   {
+      "title":"Seniors Housing Services",
+      "url":"https://www.toronto.ca/community-people/children-parenting/seniors-services/seniors-housing-services/"
+   },
+   {
+      "title":"Coordinated Access to Housing & Supports",
+      "url":"https://www.toronto.ca/community-people/community-partners/coordinated-access-to-housing-supports/"
+   },
+   {
+      "title":"Renting in Ontario: Your rights",
+      "url":"https://www.ontario.ca/page/renting-ontario-your-rights"
+   }
+]
